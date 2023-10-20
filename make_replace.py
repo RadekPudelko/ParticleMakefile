cb2a535010f0bc24e39213fc7145bf209dd1a1cd
Radek Pudelko <radek@medshift.com>
2023-10-20 14:01:21 -0400

    Script to replace old makefiles

---
+++ make_replace.py
@@ -0,0 +1,31 @@
+import os
+from os.path import isdir
+import re
+import shutil
+
+def recurse_list(top_path):
+    files = os.listdir(top_path)
+    myList = []
+    for file in files:
+        path = f"{top_path}/{file}"
+        if os.path.isdir(path):
+            myList.extend(recurse_list(path))
+        elif file == "Makefile":
+            with open(path, "r") as f:
+                line = f.readline()
+                if "### MY_PARTICLE_MAKEFILE" not in line:
+                    continue
+            myList.append(path)
+    return myList
+
+def main():
+    new_makefile = "/Users/radek/code/scripts/ParticleMakefile/Makefile"
+    top_path = "/Users/radek/code/cpp"
+    myList = recurse_list(top_path)
+    for path in myList:
+        print(path)
+        # shutil.copy(new_makefile, path)
+
+if __name__ == "__main__":
+    main()
+    
